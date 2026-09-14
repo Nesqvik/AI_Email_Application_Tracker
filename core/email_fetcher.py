5c24cebc76281faa70329ecc87cb79ef70494bc5
Natalia Ukhanova <n.ukhanova@atenekom.eu>
2026-09-14 10:57:22 +0200

    wip

--- core/email_fetcher.py
+++ core/email_fetcher.py
@@ -7,7 +7,10 @@ from googleapiclient.discovery import build
 from google.oauth2.credentials import Credentials
 
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = [
+    "https://www.googleapis.com/auth/gmail.readonly",
+    "https://www.googleapis.com/auth/gmail.send",
+]
 
 def authenticate():
     creds = None
--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from core.storage import get_all_job_emails
-
-app = FastAPI()
-
-@app.get("/emails")
-def get_emails():
-    data = get_all_job_emails()
-    return [e.__dict__ for e in data]
